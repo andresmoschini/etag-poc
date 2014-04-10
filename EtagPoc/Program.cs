f5a8e6a1f23e75541146d56170b25d3a8f24a531
Andres Moschini <andresmoschini@gmail.com>
2014-04-10 17:07:31 -0300

    Base project

---
+++ EtagPoc/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtagPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var response = GetResponse(Properties.Settings.Default.Url);
+            var lenght = GetLenght(response);
+            var content = GetContent(response);
+            Console.WriteLine("lenght: {0}", lenght);
+            Console.WriteLine("content: {0}", content);
+            Console.ReadLine();
+        }
+
+        static WebResponse GetResponse(string url)
+        {
+            var request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            return request.GetResponse();
+        }
+
+        static long GetLenght(WebResponse response)
+        {
+            return long.Parse(response.Headers["Content-Length"]);
+        }
+
+        static string GetContent(WebResponse response)
+        {
+            var stream = response.GetResponseStream();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+    }
+}
