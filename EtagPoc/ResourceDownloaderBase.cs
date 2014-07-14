6443feb7a8f526e9d98653b413a04a6c48bb2fef
Andres Moschini <andresmoschini@gmail.com>
2014-07-14 16:13:01 -0300

    Replace WebRequest by WebClient

--- EtagPoc/ResourceDownloaderBase.cs
+++ EtagPoc/ResourceDownloaderBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,6 @@ namespace EtagPoc
 {
     public abstract class ResourceDownloaderBase
     {
-        public string LastEtag { get; private set; }
-        public string LastContent { get; private set; }
-
         public readonly string Uri;
         
         protected abstract void PrepareRequest(HttpWebRequest request);
@@ -24,39 +22,10 @@ namespace EtagPoc
 
         public string GetContent()
         {
-            RevalidateLastContent();
-            return LastContent;
-        }
-
-        private void RevalidateLastContent()
-        {
-            var request = (HttpWebRequest)WebRequest.Create(Uri);
-
-            PrepareRequest(request);
-            
-            if (LastEtag != null && LastContent != null)
-            {
-                request.Headers.Add(HttpRequestHeader.IfNoneMatch, LastEtag);
-            }
-            
-            try
-            {
-                var response = (HttpWebResponse)request.GetResponse();
-                LastEtag = response.Headers[HttpResponseHeader.ETag];
-                using (Stream dataStream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(dataStream))
-                {
-                    LastContent = reader.ReadToEnd();
-                }
-            }
-            catch (WebException ex)
-            {
-                var response = ex.Response as HttpWebResponse;
-                if (response == null || response.StatusCode != HttpStatusCode.NotModified)
-                {
-                    throw;
-                }
-            }
+            var client = new System.Net.WebClient();
+            client.CachePolicy = new RequestCachePolicy(RequestCacheLevel.Revalidate);
+            var file = client.DownloadString(Uri);
+            return file;
         }
 
     }
